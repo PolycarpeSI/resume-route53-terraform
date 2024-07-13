ad25d0f78033ed4231601444b9f5b1193670f420
PolycarpeSI <psiekambe@gmail.com>
2024-07-13 23:05:42 +0200

    second

--- route53.tf
+++ route53.tf
@@ -2,6 +2,6 @@ resource "aws_route53_record" "rc1" {
   zone_id = "Z04726547GQZFK0LG2A"
   type = "A"
   ttl = 300
-  name = "resumee.polypersonnal.pro"
+  name = "resume.polypersonnal.pro"
   records = [ aws_instance.Server1.public_ip ]
 }
